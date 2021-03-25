af95b1b595d46cd3b364e35085b0bd6c938ba785
Gu7z <gustavoferri13@gmail.com>
2021-03-25 19:22:52 -0400

    update quicksort

--- magic/quickSort/invert.js
+++ magic/quickSort/invert.js
@@ -1,39 +1,52 @@
 const invert_10 = require("../../sets/invert/invert_10");
+const invert_20 = require("../../sets/invert/invert_20");
+const invert_30 = require("../../sets/invert/invert_30");
 const invert_100 = require("../../sets/invert/invert_100");
+const invert_200 = require("../../sets/invert/invert_200");
+const invert_300 = require("../../sets/invert/invert_300");
 const invert_1000 = require("../../sets/invert/invert_1000");
+const invert_2000 = require("../../sets/invert/invert_2000");
+const invert_3000 = require("../../sets/invert/invert_3000");
+const invert_10000 = require("../../sets/invert/invert_10000");
+const invert_20000 = require("../../sets/invert/invert_20000");
+const invert_30000 = require("../../sets/invert/invert_30000");
 const runCode = require("../../utils/runCode");
 const calculaMedia = require("../../utils/calculaMedia");
 const quickSort = require("../../algorithms/quickSort");
 
-let time_invert_10 = [];
-let time_invert_100 = [];
-let time_invert_1000 = [];
+const ten = [invert_10, invert_100, invert_1000, invert_10000];
+const twenty = [invert_20, invert_200, invert_2000, invert_20000];
+const thirty = [invert_30, invert_300, invert_3000, invert_30000];
 
-runCode(quickSort, invert_10, time_invert_10);
-runCode(quickSort, invert_10, time_invert_10);
-runCode(quickSort, invert_10, time_invert_10);
-const quickSort_invert_10 = calculaMedia("quickSort_invert_10", time_invert_10);
+const timeinvertTen = [[], [], [], []];
 
-runCode(quickSort, invert_100, time_invert_100);
-runCode(quickSort, invert_100, time_invert_100);
-runCode(quickSort, invert_100, time_invert_100);
-const quickSort_invert_100 = calculaMedia(
-  "quickSort_invert_100",
-  time_invert_100
-);
+const timeinvertTwenty = [[], [], [], []];
 
-runCode(quickSort, invert_1000, time_invert_1000);
-runCode(quickSort, invert_1000, time_invert_1000);
-runCode(quickSort, invert_1000, time_invert_1000);
-const quickSort_invert_1000 = calculaMedia(
-  "quickSort_invert_1000",
-  time_invert_1000
-);
+const timeinvertThirty = [[], [], [], []];
 
-const average = [
-  quickSort_invert_10,
-  quickSort_invert_100,
-  quickSort_invert_1000,
-];
+const quickSort_invert = [[], [], []];
 
-module.exports = average;
+for (const index in ten) {
+  runCode(quickSort, ten[index], timeinvertTen[index]);
+  runCode(quickSort, ten[index], timeinvertTen[index]);
+  runCode(quickSort, ten[index], timeinvertTen[index]);
+  quickSort_invert[0].push(calculaMedia(timeinvertTen[index]));
+}
+
+for (const index in twenty) {
+  runCode(quickSort, twenty[index], timeinvertTwenty[index]);
+  runCode(quickSort, twenty[index], timeinvertTwenty[index]);
+  runCode(quickSort, twenty[index], timeinvertTwenty[index]);
+  quickSort_invert[1].push(calculaMedia(timeinvertTwenty[index]));
+}
+
+for (const index in thirty) {
+  runCode(quickSort, thirty[index], timeinvertThirty[index]);
+  runCode(quickSort, thirty[index], timeinvertThirty[index]);
+  runCode(quickSort, thirty[index], timeinvertThirty[index]);
+  quickSort_invert[2].push(calculaMedia(timeinvertThirty[index]));
+}
+
+console.log("cabei o invert do bubble");
+
+module.exports = quickSort_invert;
--- magic/quickSort/random.js
+++ magic/quickSort/random.js
@@ -1,39 +1,52 @@
 const random_10 = require("../../sets/random/random_10");
+const random_20 = require("../../sets/random/random_20");
+const random_30 = require("../../sets/random/random_30");
 const random_100 = require("../../sets/random/random_100");
+const random_200 = require("../../sets/random/random_200");
+const random_300 = require("../../sets/random/random_300");
 const random_1000 = require("../../sets/random/random_1000");
+const random_2000 = require("../../sets/random/random_2000");
+const random_3000 = require("../../sets/random/random_3000");
+const random_10000 = require("../../sets/random/random_10000");
+const random_20000 = require("../../sets/random/random_20000");
+const random_30000 = require("../../sets/random/random_30000");
 const runCode = require("../../utils/runCode");
 const calculaMedia = require("../../utils/calculaMedia");
 const quickSort = require("../../algorithms/quickSort");
 
-let time_random_10 = [];
-let time_random_100 = [];
-let time_random_1000 = [];
+const ten = [random_10, random_100, random_1000, random_10000];
+const twenty = [random_20, random_200, random_2000, random_20000];
+const thirty = [random_30, random_300, random_3000, random_30000];
 
-runCode(quickSort, random_10, time_random_10);
-runCode(quickSort, random_10, time_random_10);
-runCode(quickSort, random_10, time_random_10);
-const quickSort_random_10 = calculaMedia("quickSort_random_10", time_random_10);
+const timerandomTen = [[], [], [], []];
 
-runCode(quickSort, random_100, time_random_100);
-runCode(quickSort, random_100, time_random_100);
-runCode(quickSort, random_100, time_random_100);
-const quickSort_random_100 = calculaMedia(
-  "quickSort_random_100",
-  time_random_100
-);
+const timerandomTwenty = [[], [], [], []];
 
-runCode(quickSort, random_1000, time_random_1000);
-runCode(quickSort, random_1000, time_random_1000);
-runCode(quickSort, random_1000, time_random_1000);
-const quickSort_random_1000 = calculaMedia(
-  "quickSort_random_1000",
-  time_random_1000
-);
+const timerandomThirty = [[], [], [], []];
 
-const average = [
-  quickSort_random_10,
-  quickSort_random_100,
-  quickSort_random_1000,
-];
+const quickSort_random = [[], [], []];
 
-module.exports = average;
+for (const index in ten) {
+  runCode(quickSort, ten[index], timerandomTen[index]);
+  runCode(quickSort, ten[index], timerandomTen[index]);
+  runCode(quickSort, ten[index], timerandomTen[index]);
+  quickSort_random[0].push(calculaMedia(timerandomTen[index]));
+}
+
+for (const index in twenty) {
+  runCode(quickSort, twenty[index], timerandomTwenty[index]);
+  runCode(quickSort, twenty[index], timerandomTwenty[index]);
+  runCode(quickSort, twenty[index], timerandomTwenty[index]);
+  quickSort_random[1].push(calculaMedia(timerandomTwenty[index]));
+}
+
+for (const index in thirty) {
+  runCode(quickSort, thirty[index], timerandomThirty[index]);
+  runCode(quickSort, thirty[index], timerandomThirty[index]);
+  runCode(quickSort, thirty[index], timerandomThirty[index]);
+  quickSort_random[2].push(calculaMedia(timerandomThirty[index]));
+}
+
+console.log("cabei o random do bubble");
+
+module.exports = quickSort_random;
